a975c598f3b20a77daf27fc9850dd8fb7d8cf576
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-06-02 20:46:08 +0700

    project: Install and configure ESLint dependency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+// Copyright (c) shigeru22. Licensed under the MIT license.
+// See LICENSE in the repository root for details.
+
+function outputConsole(message: string) {
+  console.log(message);
+}
+
+outputConsole("Hello, world!");
