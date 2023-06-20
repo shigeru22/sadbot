eb7fb09df0f2d14102182eb4b4b41808a4d310f4
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-06-20 15:53:33 +0700

    project: Initialize dotnet project

---
+++ SadBot.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) shigeru22. Licensed under the MIT license.
+// See LICENSE in the repository root for details.
+
+namespace SadBot.Client;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, world!");
+	}
+}
