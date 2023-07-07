8ad50d66cf982762477574228fe6c72524e5498d
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-07-07 10:45:13 +0700

    utils: Modify logging useUtc defaults to false

--- SadBot.Utils/Date.cs
+++ SadBot.Utils/Date.cs
@@ -7,19 +7,19 @@ public static class Date
 {
 	private static readonly DateTime zeroTime = new DateTime(1, 1, 1);
 
-	public static string GetCurrentDate(bool useUtc = true)
+	public static string GetCurrentDate(bool useUtc = false)
 	{
 		DateTime currentTime = useUtc ? DateTime.UtcNow : DateTime.Now;
 		return $"{currentTime.Year}/{currentTime.Month.ToString().PadLeft(2, '0')}/{currentTime.Day.ToString().PadLeft(2, '0')}";
 	}
 
-	public static string GetCurrentTime(bool useUtc = true)
+	public static string GetCurrentTime(bool useUtc = false)
 	{
 		DateTime currentTime = useUtc ? DateTime.UtcNow : DateTime.Now;
 		return $"{currentTime.Hour.ToString().PadLeft(2, '0')}:{currentTime.Minute.ToString().PadLeft(2, '0')}/{currentTime.Second.ToString().PadLeft(2, '0')}";
 	}
 
-	public static string GetCurrentDateTime(bool useUtc = true)
+	public static string GetCurrentDateTime(bool useUtc = false)
 	{
 		DateTime currentTime = useUtc ? DateTime.UtcNow : DateTime.Now;
 		return $"{currentTime.Year}/{currentTime.Month.ToString().PadLeft(2, '0')}/{currentTime.Day.ToString().PadLeft(2, '0')} {currentTime.Hour.ToString().PadLeft(2, '0')}:{currentTime.Minute.ToString().PadLeft(2, '0')}:{currentTime.Second.ToString().PadLeft(2, '0')}";
